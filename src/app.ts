977ff1f5a885ff562f5b607bf33032920add1634
Nkot117 <kumakura117@gmail.com>
2024-09-19 08:15:24 +0900

    エントリーポイントを変更

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,4 @@ env.config();
 
 import "./config/dbConfig.js";
 const app = express();
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`Server Start! PORT=${port}`);
-});
+export default app;
