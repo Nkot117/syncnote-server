86d61fe64a41bde764dc039283ba933e4d6c8a73
Nkot117 <kumakura117@gmail.com>
2024-09-19 08:45:43 +0900

    ユーザーの登録処理を追加

--- src/app.ts
+++ src/app.ts
@@ -3,5 +3,10 @@ import env from "dotenv";
 env.config();
 
 import "./config/dbConfig.js";
+import apiRoute from "./routes/index.js";
+
 const app = express();
+app.use(express.json());
+app.use("/api", apiRoute);
+
 export default app;
---
+++ src/controllers/userController.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+import CryptoJS from "crypto-js";
+
+import User from "../models/userModel.js";
+
+/**
+ * ユーザー登録処理
+ */
+async function registerUser(req: Request, res: Response) {
+  const { name, email, password } = req.body;
+
+  try {
+    const encryptedPassword = CryptoJS.AES.encrypt(
+      password,
+      process.env.SECRET_KEY || ""
+    );
+
+    const user = await User.create({
+      name,
+      email,
+      password: encryptedPassword,
+      emailVerified: false,
+    });
+
+    return res.status(200).json({ user });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "ユーザー登録に失敗しました" });
+  }
+}
+
+export { registerUser };
