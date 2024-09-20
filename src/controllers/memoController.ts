76708b517c99a68657ec2610099efd221ed3967b
Nkot117 <kumakura117@gmail.com>
2024-09-21 08:28:57 +0900

    メモコントローラーの大枠を作成

---
+++ src/controllers/memoController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+
+/**
+ * メモ新規作成
+ */
+async function createMemo(req: Request, res: Response) {}
+
+/**
+ * メモ一覧取得
+ */
+async function getMemoList(req: Request, res: Response) {}
+
+/**
+ * メモ１件取得
+ */
+async function getMemo(req: Request, res: Response) {}
+
+/**
+ * メモ更新
+ */
+async function updateMemo(req: Request, res: Response) {}
+
+/**
+ * メモ削除
+ */
+async function deleteMemo(req: Request, res: Response) {}
