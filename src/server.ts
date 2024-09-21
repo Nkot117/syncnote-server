580d141e46598a99731fbad70e9017196033e54b
Nkot117 <kumakura117@gmail.com>
2024-09-21 19:27:57 +0900

    ポートの指定をenvを参照するように変更

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from './app.js';
 
-const port = process.env.PORT || 8080;
+const port = process.env.DEV_PORT || 8080;
 app.listen(port, () => {
   console.log(`Server Start! PORT=${port}`);
 });
