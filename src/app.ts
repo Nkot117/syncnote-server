9a48e40abb6ee75423ed01761a3f11ac44fe22d5
Nkot117 <kumakura117@gmail.com>
2024-11-10 12:24:30 +0900

    CORSを設定

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import env from "dotenv";
 env.config();
 
@@ -6,6 +7,15 @@ import "./config/dbConfig.js";
 import apiRoute from "./routes/index.js";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.WEB_APP_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/api", apiRoute);
 
