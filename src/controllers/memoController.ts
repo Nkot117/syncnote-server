adb254e1335d4cf0bfda1d1c756aab0e9edd29ba
Nkot117 <kumakura117@gmail.com>
2024-11-14 23:51:02 +0900

    レスポンスを修正

--- src/controllers/memoController.ts
+++ src/controllers/memoController.ts
@@ -11,8 +11,13 @@ async function createMemo(req: Request, res: Response) {
   const { title, content } = req.body;
 
   try {
-    const memo = await Memo.create({ userId, title, content });
-    return res.status(200).json({ memo });
+    const result = await Memo.create({ userId, title, content });
+    const response = {
+      id: result._id.toString(), 
+      title: result.title, 
+      content: result.content, 
+     };
+    return res.status(200).json(response);
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "メモの新規作成に失敗しました" });
@@ -27,8 +32,20 @@ async function getMemoList(req: Request, res: Response) {
   const userId = req.user._id;
 
   try {
-    const memoList = await Memo.find({ userId });
-    return res.status(200).json({ memoList });
+    const result = await Memo.find({ userId });
+    const memoList = result.map((memo) => {
+      return { 
+        id: memo._id.toString(),
+        title: memo.title, 
+        content: memo.content, 
+      };
+    });
+
+    const response = {
+      memoList,
+    }
+
+    return res.status(200).json(response);
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "メモ一覧の取得に失敗しました" });
@@ -41,15 +58,22 @@ async function getMemoList(req: Request, res: Response) {
 async function getMemo(req: Request, res: Response) {
   console.log("getMemo called");
   const id = req.params.id;
+  const userId = req.user._id;
 
   try {
-    const memo = await Memo.findById(id);
+    const result = await Memo.findById(id);
 
-    if (!memo) {
+    if (!result) {
       return res.status(404).json({ message: "メモが見つかりません" });
     }
+
+    const response = {
+      id: result._id.toString(),
+      title: result.title,
+      content: result.content,
+    }
     
-    return res.status(200).json({ memo });
+    return res.status(200).json(response);
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "メモの取得に失敗しました" });
@@ -70,13 +94,19 @@ async function updateMemo(req: Request, res: Response) {
       return res.status(404).json({ message: "メモが見つかりません" });
     }
 
-    const updatedMemo = await Memo.findByIdAndUpdate(
+    const result = await Memo.findByIdAndUpdate(
       id,
       { $set: req.body },
       { new: true }
     );
 
-    return res.status(200).json({ memo: updatedMemo });
+    const response = {
+      id: result?._id.toString(),
+      title: result?.title,
+      content: result?.content,
+    }
+
+    return res.status(200).json(response);
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "メモの更新に失敗しました" });
